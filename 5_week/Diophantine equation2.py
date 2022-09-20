1006fb8a027fda488b189dbabdb6e21efe341935
Dudermont <dudermont@gmail.com>
2022-09-20 03:56:32 +0300

    Create Diophantine equation2.py

---
+++ 5_week/Diophantine equation2.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+count = 0
+
+for x in range(1001):
+    if x != e:
+        if a * x ** 3 + b * x ** 2 + c * x + d == 0:
+            count += 1
+print(count)
