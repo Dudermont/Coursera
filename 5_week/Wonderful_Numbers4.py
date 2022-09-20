1278e3cba861406456a8503d30333df3ca914406
Dudermont <dudermont@gmail.com>
2022-09-20 05:42:09 +0300

    Update Wonderful_Numbers4.py

--- 5_week/Wonderful_Numbers4.py
+++ 5_week/Wonderful_Numbers4.py
@@ -6,6 +6,5 @@ for i in range(a, b + 1):
     second = (i % 1000) // 100
     third = ((i % 1000) % 100) // 10
     last = ((i % 1000) % 100) % 10
-
     if first == last and second == third:
         print(i)
