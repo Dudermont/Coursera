5447203b1fd7626939898c7234c294a2326f135e
Dudermont <dudermont@gmail.com>
2022-09-20 05:07:03 +0300

    Create Wonderful_Numbers4.py

---
+++ 5_week/Wonderful_Numbers4.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+
+for i in range(a, b + 1):
+    first = i // 1000
+    second = (i % 1000) // 100
+    third = ((i % 1000) % 100) // 10
+    last = ((i % 1000) % 100) % 10
+
+    if first == last and second == third:
+        print(i)
