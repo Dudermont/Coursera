ae1e8c800c7cf7068f8e52061a9eb43f46157b44
Dudermont <dudermont@gmail.com>
2023-01-10 23:23:36 +0300

    Create Partial_sums.py

---
+++ 8_week/Partial_sums.py
@@ -0,0 +1,9 @@
+import itertools
+print(
+    *itertools.accumulate(
+                            map(
+                                int,
+                                input().split()
+                            )
+    )
+)
