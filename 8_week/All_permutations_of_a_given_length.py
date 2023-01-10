95a52e76ea561e405a31a565906dd7505c999739
Dudermont <dudermont@gmail.com>
2023-01-10 23:22:54 +0300

    Create All_permutations_of_a_given_length.py

---
+++ 8_week/All_permutations_of_a_given_length.py
@@ -0,0 +1,18 @@
+import itertools
+print(
+    *map(
+        lambda x: ''.join(x),
+        map(
+            list,
+            itertools.permutations(
+                map(
+                    str,
+                    range(
+                        1,
+                        int(input()) + 1)
+                )
+            )
+        )
+    ),
+    sep='\n'
+)
