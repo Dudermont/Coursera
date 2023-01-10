ff45afe77a9974c3f1c4fa9deedd5ae085b7f1ec
Dudermont <dudermont@gmail.com>
2023-01-10 23:24:21 +0300

    Create Zero_or_not_zero.py

---
+++ 8_week/Zero_or_not_zero.py
@@ -0,0 +1,14 @@
+print(
+    any(
+        map(
+            lambda x: int(input()) == 0,
+            list(
+                range(
+                    int(
+                        input()
+                    )
+                )
+            )
+        )
+    )
+)
