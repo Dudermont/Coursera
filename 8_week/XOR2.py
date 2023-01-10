5594ce412b80e2efa405b1cd6245fad75007c254
Dudermont <dudermont@gmail.com>
2023-01-10 23:24:07 +0300

    Create XOR2.py

---
+++ 8_week/XOR2.py
@@ -0,0 +1,18 @@
+from functools import reduce
+print(
+    *reduce(
+        lambda x, y: map(
+            lambda a, b: a ^ b, x, y),
+        map(
+            lambda x: map(
+                int,
+                input().split()
+            ),
+            range(
+                int(
+                    input()
+                )
+            )
+        )
+    )
+)
